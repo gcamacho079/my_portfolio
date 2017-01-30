9f97e26b0cd7ddc34fdeff7fba9a93bc211334aa
Guadalupe Camacho <gcamacho079@gmail.com>
2017-01-30 09:37:39 -0800

    Replace flex column with float and clearfix solution

---
+++ js/script.js
@@ -0,0 +1,5 @@
+/*
+"Science, my lad, is made up of mistakes, but they are mistakes which it is useful to make, because they lead little by little to the truth." -Jules Verne
+
+"Every time I see an adult on a bicycle, I no longer despair for the future of the human race." -HG Wells
+*/
