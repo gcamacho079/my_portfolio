b7181fbc159545306e81818bfb5e83b8186cfa0b
Guadalupe Camacho <gcamacho079@gmail.com>
2017-01-30 20:12:58 -0800

    Remove icon for more quotes until js is done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-/*
+/* QUOTES TO CYCLE THROUGH
 "Science, my lad, is made up of mistakes, but they are mistakes which it is useful to make, because they lead little by little to the truth." -Jules Verne
 
-"Every time I see an adult on a bicycle, I no longer despair for the future of the human race." -HG Wells
+"It's the questions we can't answer that teach us the most. They teach us how to think. If you give a man an answer, all he gains is a little fact. But give him a question and he'll look for his own answers."
 */
