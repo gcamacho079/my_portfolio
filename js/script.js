75d5319d57bdeb0d020e23fe330b86edf6a897f2
Guadalupe Camacho <gcamacho079@gmail.com>
2017-01-31 10:19:29 -0800

    Add smooth scrolling

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-/* QUOTES TO CYCLE THROUGH
-"Science, my lad, is made up of mistakes, but they are mistakes which it is useful to make, because they lead little by little to the truth." -Jules Verne
+$(document).ready(function() {
+  $('a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
 
-"It's the questions we can't answer that teach us the most. They teach us how to think. If you give a man an answer, all he gains is a little fact. But give him a question and he'll look for his own answers."
-*/
+    var target = this.hash; // Stores hash as target
+    var $target = $(target); // Turns target dest. into a jQuery object
+
+    $('html, body').animate({
+      'scrollTop': $target.offset().top
+    }, 1200, 'swing', function() { 
+      window.location.hash = target;
+    });
+
+
+  });
+});
